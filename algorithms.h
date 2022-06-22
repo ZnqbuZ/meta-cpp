ea4cd60d2d2107a664e5c4a26c07ac606d1f0bcb
ZnqbuZ <ZnqbuZ+GitHub@gmail.com>
2022-06-22 21:09:02 +0800

    Change Encoding

--- algorithms.h
+++ algorithms.h
@@ -1,386 +1,324 @@
 #pragma once
 
-#include "basic_types.h"
+#include "types.h"
 #include "arithmetics.h"
 
 using is_greater_equal = C(Not, is_less);
 using is_less_equal = C(Not, is_greater);
 
-struct swap
-{
+struct swap {
     FUNC_HEAD_THROW(swap);
 
-    template <typename T, typename V>
-    struct apply_on<L(T, V)>
-    {
+    template<typename T, typename V>
+    struct apply_on<L(T, V) > {
         using ret = L(Id(V), Id(T));
     };
 };
 
-struct do_if
-{
+struct do_if {
     FUNC_HEAD_THROW(do_if);
 
-    template <
-        is::delayed cond,
-        is::function THEN, typename THEN_arg_list,
-        is::function ELSE, typename ELSE_arg_list>
-    struct apply_on<L(cond, THEN, THEN_arg_list, ELSE, ELSE_arg_list)>
-    {
+    template<
+            is::delayed cond,
+            is::function THEN, typename THEN_arg_list,
+            is::function ELSE, typename ELSE_arg_list>
+    struct apply_on<L(cond, THEN, THEN_arg_list, ELSE, ELSE_arg_list) > {
         using ret = D(do_if, cond, THEN, THEN_arg_list, ELSE, ELSE_arg_list);
     };
 
-    template <
-        is::bool_type cond,
-        is::function THEN, typename THEN_arg_list,
-        is::function ELSE, typename ELSE_arg_list>
-    requires(Value(cond)) struct apply_on<L(cond, THEN, THEN_arg_list, ELSE, ELSE_arg_list)>
-    {
+    template<
+            is::bool_type cond,
+            is::function THEN, typename THEN_arg_list,
+            is::function ELSE, typename ELSE_arg_list> requires(Value(cond))
+    struct apply_on<L(cond, THEN, THEN_arg_list, ELSE, ELSE_arg_list) > {
         using ret = Ret(THEN, THEN_arg_list);
     };
 
-    template <
-        is::bool_type cond,
-        is::function THEN, typename THEN_arg_list,
-        is::function ELSE, typename ELSE_arg_list>
-    requires(!Value(cond)) struct apply_on<L(cond, THEN, THEN_arg_list, ELSE, ELSE_arg_list)>
-    {
+    template<
+            is::bool_type cond,
+            is::function THEN, typename THEN_arg_list,
+            is::function ELSE, typename ELSE_arg_list> requires (!Value(cond))
+    struct apply_on<L(cond, THEN, THEN_arg_list, ELSE, ELSE_arg_list) > {
         using ret = Ret(ELSE, ELSE_arg_list);
     };
 };
 
-struct push_front
-{
+struct push_front {
     FUNC_HEAD_THROW(push_front);
 
-    template <typename T, typename V>
-    struct apply_on<L(T, V)>
-    {
+    template<typename T, typename V>
+    struct apply_on<L(T, V) > {
         using ret = L(Id(T), Id(V));
     };
 
-    template <typename T, is::delayed V>
-    struct apply_on<L(T, V)>
-    {
+    template<typename T, is::delayed V>
+    struct apply_on<L(T, V) > {
         using ret = D(push_front, T, V);
     };
 
-    template <typename T, is::list V>
-    struct apply_on<L(T, V)>
-    {
+    template<typename T, is::list V>
+    struct apply_on<L(T, V) > {
         using ret = Ret(typename V::push_front, T);
     };
 };
 
-struct push_back
-{
+struct push_back {
     FUNC_HEAD_THROW(push_back);
 
-    template <typename T, typename V>
-    struct apply_on<L(T, V)>
-    {
+    template<typename T, typename V>
+    struct apply_on<L(T, V) > {
         using ret = L(Id(V), Id(T));
     };
 
-    template <typename T, is::delayed V>
-    struct apply_on<L(T, V)>
-    {
+    template<typename T, is::delayed V>
+    struct apply_on<L(T, V) > {
         using ret = D(push_back, T, V);
     };
 
-    template <typename T, is::list V>
-    struct apply_on<L(T, V)>
-    {
+    template<typename T, is::list V>
+    struct apply_on<L(T, V) > {
         using ret = Ret(typename V::push_back, T);
     };
 };
 
-struct join
-{
+struct join {
     FUNC_HEAD_THROW(join);
 
-    template <typename T, typename V>
-    struct apply_on<L(T, V)>
-    {
+    template<typename T, typename V>
+    struct apply_on<L(T, V) > {
         using ret = L(Id(T), Id(V));
     };
 
     WAIT_FOR_n_DELAYED_ARGS(join, 2);
 
-    template <is::not_delayed T, is::list l>
-    struct apply_on<L(T, l)>
-    {
+    template<is::not_delayed T, is::list l>
+    struct apply_on<L(T, l) > {
         using ret = Ret(push_front, T, l);
     };
 
-    template <is::list l, is::not_delayed V>
-    struct apply_on<L(l, V)>
-    {
+    template<is::list l, is::not_delayed V>
+    struct apply_on<L(l, V) > {
         using ret = Ret(push_back, V, l);
     };
 
-    template <typename... Ts, typename... Vs>
-    struct apply_on<L(L(Ts...), L(Vs...))>
-    {
+    template<typename... Ts, typename... Vs>
+    struct apply_on<L(L(Ts...), L(Vs...))> {
         using ret = L(Ts..., Vs...);
     };
 };
 
-struct peek_front
-{
+struct peek_front {
     FUNC_HEAD_ID;
     WAIT_FOR_DELAYED_ARGS(peek_front);
 
-    template <is::list V>
-    struct apply_on<V>
-    {
+    template<is::list V>
+    struct apply_on<V> {
         using ret = Ret(typename V::peek_front, void);
     };
 };
 
-struct peek_back
-{
+struct peek_back {
     FUNC_HEAD_ID;
     WAIT_FOR_DELAYED_ARGS(peek_back);
 
-    template <is::list V>
-    struct apply_on<V>
-    {
+    template<is::list V>
+    struct apply_on<V> {
         using ret = Ret(typename V::peek_back, void);
     };
 };
 
-struct pop_front
-{
+struct pop_front {
     FUNC_HEAD_THROW(pop_front);
 
-    template <is::list V>
-    struct apply_on<V>
-    {
+    template<is::list V>
+    struct apply_on<V> {
         using ret = Ret(typename V::pop_front, void);
     };
 };
 
-struct pop_back
-{
+struct pop_back {
     FUNC_HEAD_THROW(pop_back);
 
-    template <is::list V>
-    struct apply_on<V>
-    {
+    template<is::list V>
+    struct apply_on<V> {
         using ret = Ret(typename V::pop_back, void);
     };
 };
 
-struct insert_l
-{
+struct insert_l {
     FUNC_HEAD_THROW(insert_l);
 
     struct ERROR_INDEX_OUT_OF_RANGE;
-    template <typename T, is::arg Index, is::list l>
-    struct apply_on<L(T, l, Index)>
-    {
+    template<typename T, is::arg Index, is::list l>
+    struct apply_on<L(T, l, Index) > {
         using ret = ERROR_INDEX_OUT_OF_RANGE;
     };
 
-    template <typename T, is::arg Index, typename T1, typename... Ts>
-    requires(
-        RetV(is_greater, Index, A(0)) &&
-        RetV(is_less, Index, A(1 + sizeof...(Ts)))) struct apply_on<L(T, L(T1, Ts...), Index)>
-    {
+    template<typename T, is::arg Index, typename T1, typename... Ts> requires (
+            RetV(is_greater, Index, A(0)) &&
+            RetV(is_less, Index, A(1 + sizeof...(Ts))))
+    struct apply_on<L(T, L(T1, Ts...), Index) > {
         using ret = Ret(push_front, T1, Ret(insert_l, T, Id(Ts...), DEC(Index)));
     };
 
-    template <typename T, is::list l>
-    struct apply_on<L(T, l, A(0))>
-    {
+    template<typename T, is::list l>
+    struct apply_on<L(T, l, A(0))> {
         using ret = Ret(push_front, T, l);
     };
 
-    template <typename T, is::list l>
-    struct apply_on<L(T, l, Len(l))>
-    {
+    template<typename T, is::list l>
+    struct apply_on<L(T, l, Len(l))> {
         using ret = Ret(push_back, T, l);
     };
 
-    template <typename T, typename T1, typename T2>
-    struct apply_on<L(T, L(T1, T2), A(1))>
-    {
+    template<typename T, typename T1, typename T2>
+    struct apply_on<L(T, L(T1, T2), A(1))> {
         using ret = L(Id(T1), Id(T), Id(T2));
     };
 };
 
-struct remove_l
-{
+struct remove_l {
     FUNC_HEAD_THROW(remove_l);
 
     struct ERROR_INDEX_OUT_OF_RANGE;
-    template <is::arg Index, is::list l>
-    struct apply_on<L(l, Index)>
-    {
+    template<is::arg Index, is::list l>
+    struct apply_on<L(l, Index) > {
         using ret = ERROR_INDEX_OUT_OF_RANGE;
     };
 
-    template <is::arg Index, typename T1, typename... Ts>
-    requires(
-        RetV(is_greater, Index, A(0)) &&
-        RetV(is_less, Index, A(sizeof...(Ts)))) struct apply_on<L(L(T1, Ts...), Index)>
-    {
+    template<is::arg Index, typename T1, typename... Ts> requires (
+            RetV(is_greater, Index, A(0)) &&
+            RetV(is_less, Index, A(sizeof...(Ts))))
+    struct apply_on<L(L(T1, Ts...), Index) > {
         using ret = Ret(push_front, T1, Ret(remove_l, Id(Ts...), DEC(Index)));
     };
 
-    template <is::list l>
-    struct apply_on<L(l, A(0))>
-    {
+    template<is::list l>
+    struct apply_on<L(l, A(0))> {
         using ret = Ret(pop_front, l);
     };
 
-    template <is::list l>
-    struct apply_on<L(l, DEC(Len(l)))>
-    {
+    template<is::list l>
+    struct apply_on<L(l, DEC(Len(l)))> {
         using ret = Ret(pop_back, l);
     };
 
-    template <typename T1, typename T, typename T2>
-    struct apply_on<L(L(T1, T, T2), A(1))>
-    {
+    template<typename T1, typename T, typename T2>
+    struct apply_on<L(L(T1, T, T2), A(1))> {
         using ret = L(Id(T1), Id(T2));
     };
 };
 
-struct replace_l
-{
+struct replace_l {
     FUNC_HEAD_THROW(replace_l);
 
     struct ERROR_INDEX_OUT_OF_RANGE;
-    template <typename T, is::arg Index, is::list l>
-    struct apply_on<L(T, l, Index)>
-    {
+    template<typename T, is::arg Index, is::list l>
+    struct apply_on<L(T, l, Index) > {
         using ret = ERROR_INDEX_OUT_OF_RANGE;
     };
 
-    template <typename T, is::arg Index, is::list l>
-    requires(
-        RetV(is_greater_equal, Index, A(0)) &&
-        RetV(is_less, Index, Len(l))) struct apply_on<L(T, l, Index)>
-    {
+    template<typename T, is::arg Index, is::list l> requires (
+            RetV(is_greater_equal, Index, A(0)) &&
+            RetV(is_less, Index, Len(l)))
+    struct apply_on<L(T, l, Index) > {
         using ret = Ret(remove_l, Ret(insert_l, T, l, Index), INC(Index));
     };
 };
 
-struct bind
-{
+struct bind {
 private:
-    template <is::list l>
+    template<is::list l>
     struct __1_arg;
 
 public:
-    struct take_as_1_arg
-    {
+    struct take_as_1_arg {
         FUNC_HEAD_THROW(take_as_1_arg);
 
-        template <is::list l>
-        struct apply_on<l>
-        {
+        template<is::list l>
+        struct apply_on<l> {
             using ret = __1_arg<l>;
         };
     };
 
     FUNC_HEAD_THROW(bind);
 
-    template <typename T, is::arg N>
-    struct mask_list
-    {
-        template <typename V>
-        struct apply_on
-        {
+    template<typename T, is::arg N>
+    struct mask_list {
+        template<typename V>
+        struct apply_on {
             using ret = L(T, V);
         };
 
-        template <typename V>
-        requires(RetV(is_greater, N, A(0))) struct apply_on<V>
-        {
+        template<typename V> requires(RetV(is_greater, N, A(0)))
+        struct apply_on<V> {
             using ret = L(V, T);
         };
 
-        template <is::list V>
-        struct apply_on<__1_arg<V>>
-        {
+        template<is::list V>
+        struct apply_on<__1_arg<V>> {
             using ret = L(T, V);
         };
 
-        template <is::list V>
-        requires(RetV(is_greater, N, A(0))) struct apply_on<__1_arg<V>>
-        {
+        template<is::list V> requires(RetV(is_greater, N, A(0)))
+        struct apply_on<__1_arg<V>> {
             using ret = L(V, T);
         };
 
-        template <is::list arg_list>
-        struct apply_on<arg_list>
-        {
+        template<is::list arg_list>
+        struct apply_on<arg_list> {
             using ret = Ret(insert_l, T, arg_list, N);
         };
     };
 
-    template <is::function f, typename T, is::arg N>
-    struct apply_on<L(f, T, N)>
-    {
+    template<is::function f, typename T, is::arg N>
+    struct apply_on<L(f, T, N) > {
         using ret = C(f, mask_list<T, N>);
     };
 };
 
-struct flatten
-{
+struct flatten {
     FUNC_HEAD_ID;
     WAIT_FOR_DELAYED_ARGS(flatten);
 
-    template <typename T, typename... Ts>
-    struct apply_on<L(T, Ts...)>
-    {
+    template<typename T, typename... Ts>
+    struct apply_on<L(T, Ts...) > {
         using ret = Ret(join, Ret(flatten, T), Ret(flatten, Id(Ts...)));
     };
 };
 
-struct map
-{
+struct map {
     FUNC_HEAD_THROW(map);
     //WAIT_FOR_n_DELAYED_ARGS(map, 2);
 
-    template <is::function f, typename T>
-    struct apply_on<L(f, T)>
-    {
+    template<is::function f, typename T>
+    struct apply_on<L(f, T) > {
         using ret = Ret(f, T);
     };
 
-    template <is::function f, typename... Ts>
-    struct apply_on<L(f, L(Ts...))>
-    {
+    template<is::function f, typename... Ts>
+    struct apply_on<L(f, L(Ts...))> {
         using ret = L(Ret(f, Ts)...);
     };
 };
 
-struct deep_map
-{
+struct deep_map {
     FUNC_HEAD_THROW(deep_map);
     //WAIT_FOR_n_DELAYED_ARGS(deep_map, 2);
 
-    template <is::function f, typename T>
-    struct apply_on<L(f, T)>
-    {
+    template<is::function f, typename T>
+    struct apply_on<L(f, T) > {
     private:
-        struct on
-        {
-            template <typename V>
-            struct apply_on
-            {
+        struct on {
+            template<typename V>
+            struct apply_on {
                 using ret = Ret(f, V);
             };
 
             //WAIT_FOR_DELAYED_ARGS(on);
 
-            template <typename... Vs>
-            struct apply_on<L(Vs...)>
-            {
+            template<typename... Vs>
+            struct apply_on<L(Vs...) > {
                 using ret = L(Ret(on, Vs)...);
             };
         };
@@ -390,47 +328,39 @@ struct deep_map
     };
 };
 
-struct filter
-{
+struct filter {
     struct Null;
 
     friend struct deep_filter;
     friend struct filter_s;
 
 private:
-    struct __check
-    {
+    struct __check {
         FUNC_HEAD_THROW(__check);
 
-        template <is::function f, typename T>
-        struct apply_on<L(f, T)>
-        {
+        template<is::function f, typename T>
+        struct apply_on<L(f, T) > {
             using ret = Select(Ret(cast, Ret(f, T), bool), T, Null);
         };
     };
 
-    struct __trunc
-    {
+    struct __trunc {
         FUNC_HEAD_THROW(__trunc);
 
-        template <is::list l>
-        requires(RetV(is_greater, Len(l), A(2))) struct apply_on<l>
-        {
+        template<is::list l> requires(RetV(is_greater, Len(l), A(2)))
+        struct apply_on<l> {
             using ret = Ret(C(pop_back, pop_back), l);
         };
 
-        template <is::list l>
-        requires(RetV(is_value_equal, Len(l), A(2))) struct apply_on<l>
-        {
+        template<is::list l> requires(RetV(is_value_equal, Len(l), A(2)))
+        struct apply_on<l> {
             using ret = Null;
         };
     };
 
-    struct __filter
-    {
+    struct __filter {
     private:
-        struct __shrink
-        {
+        struct __shrink {
             friend __filter;
 
         private:
@@ -439,9 +369,8 @@ private:
         public:
             FUNC_HEAD_THROW(__shrink);
 
-            template <is::list l>
-            struct apply_on<l>
-            {
+            template<is::list l>
+            struct apply_on<l> {
                 using curr = Ret(peek_front, l);
                 using rest = Ret(__shrink, Ret(pop_front, l));
 
@@ -450,9 +379,8 @@ private:
                                 push_front, L(curr, rest));
             };
 
-            template <is::list l>
-            requires(RetV(is_same, Ret(peek_front, l), STOP)) struct apply_on<l>
-            {
+            template<is::list l> requires(RetV(is_same, Ret(peek_front, l), STOP))
+            struct apply_on<l> {
                 using ret = l;
             };
         };
@@ -460,12 +388,11 @@ private:
     public:
         FUNC_HEAD_ID;
 
-        template <is::list l>
-        struct apply_on<l>
-        {
+        template<is::list l>
+        struct apply_on<l> {
             using ret = Ret(
-                C(__trunc, __shrink),
-                Ret(join, l, L(__shrink::STOP, __shrink::STOP)));
+                    C(__trunc, __shrink),
+                    Ret(join, l, L(__shrink::STOP, __shrink::STOP)));
         };
     };
 
@@ -473,15 +400,13 @@ public:
     FUNC_HEAD_THROW(filter);
     WAIT_FOR_n_DELAYED_ARGS(filter, 2);
 
-    template <is::function f, typename T>
-    struct apply_on<L(f, T)>
-    {
+    template<is::function f, typename T>
+    struct apply_on<L(f, T) > {
         using ret = Ret(__check, f, T);
     };
 
-    template <is::function f, is::list l>
-    struct apply_on<L(f, l)>
-    {
+    template<is::function f, is::list l>
+    struct apply_on<L(f, l) > {
     private:
         using __check_with_f = Ret(bind, __check, f, A(0));
 
@@ -490,19 +415,16 @@ public:
     };
 };
 
-struct deep_filter
-{
+struct deep_filter {
 private:
     using __check = filter::__check;
     using __filter = filter::__filter;
 
-    struct __deep_filter
-    {
+    struct __deep_filter {
         FUNC_HEAD_ID;
 
-        template <is::list l>
-        struct apply_on<l>
-        {
+        template<is::list l>
+        struct apply_on<l> {
             using ret = Ret(__filter, Ret(map, __deep_filter, l));
         };
     };
@@ -511,15 +433,13 @@ public:
     FUNC_HEAD_THROW(deep_filter);
     WAIT_FOR_n_DELAYED_ARGS(deep_filter, 2);
 
-    template <is::function f, typename T>
-    struct apply_on<L(f, T)>
-    {
+    template<is::function f, typename T>
+    struct apply_on<L(f, T) > {
         using ret = Ret(__check, f, T);
     };
 
-    template <is::function f, is::list l>
-    struct apply_on<L(f, l)>
-    {
+    template<is::function f, is::list l>
+    struct apply_on<L(f, l) > {
     private:
         using __check_with_f = Ret(bind, __check, f, A(0));
 
--- stream.h
+++ stream.h
@@ -1,55 +1,46 @@
 #pragma once
 
-#include "basic_types.h"
+#include "types.h"
 #include "algorithms.h"
 
-struct force
-{
-    template <is::delayed d>
+struct force {
+    template<is::delayed d>
     struct avoid;
 
     FUNC_HEAD_ID;
 
-    template <is::delayed d>
-    struct apply_on<d>
-    {
+    template<is::delayed d>
+    struct apply_on<d> {
         using ret = Ret(Ret(force, get::d_func<d>), Ret(map, force, get::d_args<d>));
     };
 };
 
-template <typename f, typename... args>
-struct delayed
-{
+template<typename f, typename... args>
+struct delayed {
     using __func = f;
     using __args = Id(args...);
 
-    template <typename T>
-    struct apply_on
-    {
+    template<typename T>
+    struct apply_on {
         using ret = D(D(f, args...), Id(T));
     };
 };
 
-namespace is
-{
-    template <typename T>
+namespace is {
+    template<typename T>
     concept stream =
-        list<T> &&
-            delayed<Ret(peek_back, T)>;
+    list<T> &&
+    delayed<Ret(peek_back, T)>;
 }
 
-struct make::stream
-{
+struct make::stream {
     FUNC_HEAD_THROW(stream);
 
-    template <is::function next, typename... Ts>
-    struct apply_on<L(next, Ts...)>
-    {
-        struct find_next
-        {
-            template <typename l>
-            struct apply_on
-            {
+    template<is::function next, typename... Ts>
+    struct apply_on<L(next, Ts...) > {
+        struct find_next {
+            template<typename l>
+            struct apply_on {
                 using next_item = Ret(next, l);
                 using next_args = Ret(pop_front, Ret(push_back, next_item, l));
                 using ret = L(next_item, D(find_next, next_args));
@@ -59,14 +50,11 @@ struct make::stream
         using ret = L(Ts..., D(find_next, Ts...));
     };
 
-    template <is::function next, typename T>
-    struct apply_on<L(next, T)>
-    {
-        struct find_next
-        {
-            template <typename l>
-            struct apply_on
-            {
+    template<is::function next, typename T>
+    struct apply_on<L(next, T) > {
+        struct find_next {
+            template<typename l>
+            struct apply_on {
                 using next_item = Ret(next, l);
                 using ret = L(next_item, D(find_next, next_item));
             };
@@ -76,54 +64,46 @@ struct make::stream
     };
 };
 
-struct calc
-{
+struct calc {
     FUNC_HEAD_THROW(calc);
 
     WAIT_FOR_n_DELAYED_ARGS(calc, 2);
 
-    template <is::not_delayed s, is::not_delayed times>
-    requires(is::stream<s> &&is::arg<times>) struct apply_on<L(s, times)>
-    {
+    template<is::not_delayed s, is::not_delayed times> requires (is::stream<s> && is::arg<times>)
+    struct apply_on<L(s, times) > {
         using ret = Ret(
-            calc,
-            Ret(
-                Select(Ret(is_greater, Len(s), A(2)), join, push_front),
-                L(Ret(pop_back, s), Id(F(Ret(peek_back, s))))),
-            DEC(times));
+                calc,
+                Ret(
+                        Select(Ret(is_greater, Len(s), A(2)), join, push_front),
+                        L(Ret(pop_back, s), Id(F(Ret(peek_back, s))))),
+                DEC(times));
     };
 
-    template <is::stream s>
-    struct apply_on<L(s, A(0))>
-    {
+    template<is::stream s>
+    struct apply_on<L(s, A(0))> {
         using ret = s;
     };
 };
 
-struct map_s
-{
+struct map_s {
     FUNC_HEAD_THROW(map_s);
     WAIT_FOR_n_DELAYED_ARGS(map_s, 2);
 
-    template <is::function f, is::stream s>
-    struct apply_on<L(f, s)>
-    {
+    template<is::function f, is::stream s>
+    struct apply_on<L(f, s) > {
         using ret = Ret(
-            Select(Ret(is_greater, Len(s), A(2)), push_back, swap),
-            Id(Ret(map_s, f, Ret(peek_back, s))), Ret(map, f, Ret(pop_back, s)));
+                Select(Ret(is_greater, Len(s), A(2)), push_back, swap),
+                Id(Ret(map_s, f, Ret(peek_back, s))), Ret(map, f, Ret(pop_back, s)));
     };
 };
 
-struct filter_s
-{
+struct filter_s {
 private:
-    struct find_filtered_next
-    {
+    struct find_filtered_next {
         FUNC_HEAD_THROW_NO_WAIT;
 
-        template <is::function f, typename find_next>
-        struct apply_on<L(f, force::avoid<find_next>)>
-        {
+        template<is::function f, typename find_next>
+        struct apply_on<L(f, force::avoid<find_next>)> {
             using next_list = F(find_next);
             using next_item = Ret(peek_front, next_list);
             using next_finder = force::avoid<Ret(peek_back, next_list)>;
@@ -139,9 +119,8 @@ private:
 public:
     FUNC_HEAD_THROW(filter_s);
 
-    template <is::function f, is::stream s>
-    struct apply_on<L(f, s)>
-    {
+    template<is::function f, is::stream s>
+    struct apply_on<L(f, s) > {
         using filtered = Ret(filter, f, Ret(pop_back, s));
         using finder = Ret(peek_back, s);
 
